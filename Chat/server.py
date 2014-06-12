da2fc4b5fb4dada023e8327604b81417e62bb0b4
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-12 20:30:46 +0300

    Initial commit-2

---
+++ Chat/server.py
@@ -0,0 +1,25 @@
+from twisted.internet import protocol, reactor
+
+transports = set()
+
+
+class Chat(protocol.Protocol):
+    def dataReceived(self, data):
+        transports.add(self.transport)
+
+        if ':' not in data:
+            return
+
+        user, msg = data.split(':')
+
+        for t in transports:
+            if t is not self.transport:
+                t.write('{} says: {}'.format(user, msg))
+
+
+class ChatFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Chat()
+
+reactor.listenTCP(9096, ChatFactory())
+reactor.run()
