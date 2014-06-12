a45cc180a680df4d71577baff47e74dc65abc500
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-12 23:31:16 +0300

    Roboto

--- Clock/app.py
+++ Clock/app.py
@@ -1,8 +1,12 @@
 from kivy.app import App
+from kivy.core.text import LabelBase
 
 
 class ClockApp(App):
     pass
 
 if __name__ == '__main__':
+    LabelBase.register(name='Roboto',
+                       fn_regular='Roboto-Thin.ttf',
+                       fn_bold='Roboto-Medium.ttf')
     ClockApp().run()
