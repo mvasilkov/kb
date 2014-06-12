3b4d79805d33d2d91cc85fb719fc82cdd57f66e7
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-12 23:46:46 +0300

    Clock-2

--- Clock/app.py
+++ Clock/app.py
@@ -1,9 +1,17 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.text import LabelBase
+
+from time import strftime
 
 
 class ClockApp(App):
-    pass
+    def on_start(self):
+        self.update()
+        Clock.schedule_interval(self.update, 1)
+
+    def update(self, *args):
+        self.root.ids.time.text = strftime('[b]%I[/b]:%M:%S')
 
 if __name__ == '__main__':
     LabelBase.register(name='Roboto',
