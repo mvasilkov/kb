efaea6604034d0f7cd80804b63f34dc64547cf68
Mark Vasilkov <mvasilkov@gmail.com>
2014-06-14 17:27:38 +0300

    Stopwatch

--- Clock/app.py
+++ Clock/app.py
@@ -8,12 +8,32 @@ from time import strftime
 
 
 class ClockApp(App):
-    def on_start(self):
-        self.update()
-        Clock.schedule_interval(self.update, 1)
+    sw_started = False
+    sw_seconds = 0
 
-    def update(self, *args):
+    def on_start(self):
+        Clock.schedule_interval(self.update, 0)
+
+    def update(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+
         self.root.ids.time.text = strftime('[b]%I[/b]:%M:%S')
+
+        m, s = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = ('%02d:%02d.[size=40]%02d[/size]' %
+                                        (int(m), int(s), int(s * 100 % 100)))
+
+    def start_stop(self):
+        self.root.ids.start_stop.text = 'Start' if self.sw_started else 'Stop'
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+
+        self.sw_seconds = 0
 
 if __name__ == '__main__':
     Window.clearcolor = get_color_from_hex('#2a2430')
