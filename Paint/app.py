6c0cee880052b6d854b01c1fe9f11292b2338c53
Mark Vasilkov <mvasilkov@gmail.com>
2014-07-05 22:51:01 +0300

    Changes

--- Paint/app.py
+++ Paint/app.py
@@ -1,9 +1,18 @@
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.graphics import Color, Line
 from kivy.uix.widget import Widget
+from kivy.utils import get_color_from_hex
 
 
 class CanvasWidget(Widget):
-    pass
+    def on_touch_down(self, touch):
+        if Widget.on_touch_down(self, touch):
+            return
+
+        with self.canvas:
+            Color(*get_color_from_hex('#0080ff80'))
+            Line(circle=(touch.x, touch.y, 25), width=4)
 
 
 class PaintApp(App):
@@ -11,4 +20,5 @@ class PaintApp(App):
         return CanvasWidget()
 
 if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex('#ffffff')
     PaintApp().run()
