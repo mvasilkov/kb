6f130afc6f75a47961519a5a1816c19c89eefd95
Mark Vasilkov <mvasilkov@gmail.com>
2014-07-05 22:28:18 +0300

    Changes

--- Paint/app.py
+++ Paint/app.py
@@ -1,8 +1,14 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasWidget(Widget):
+    pass
 
 
 class PaintApp(App):
-    pass
+    def build(self):
+        return CanvasWidget()
 
 if __name__ == '__main__':
     PaintApp().run()
