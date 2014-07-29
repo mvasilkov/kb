29a0864e442fbf3097a9f54cc54a6fcc05226b58
Mark Vasilkov <mvasilkov@gmail.com>
2014-07-29 16:28:28 +0300

    Recorder

---
+++ Recorder/app.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.config import Config
+
+
+class RecorderApp(App):
+    pass
+
+if __name__ == '__main__':
+    Config.set('graphics', 'width', '600')
+    Config.set('graphics', 'height', '900')
+
+    RecorderApp().run()
