4b67db5d00feab9d9589a341e52130468fe7ccc5
Mark Vasilkov <mvasilkov@gmail.com>
2014-07-31 22:11:07 +0300

    Changes

--- Recorder/app.py
+++ Recorder/app.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.config import Config
+from kivy.core.text import LabelBase
 
 
 class RecorderApp(App):
@@ -8,5 +9,8 @@ class RecorderApp(App):
 if __name__ == '__main__':
     Config.set('graphics', 'width', '600')
     Config.set('graphics', 'height', '900')
+
+    LabelBase.register(name='Modern Pictograms',
+                       fn_regular='modernpics.ttf')
 
     RecorderApp().run()
