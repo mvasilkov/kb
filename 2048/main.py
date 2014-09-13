e03fac2220beabc6324ea2fdbb5911b2f9b6b92e
Mark Vasilkov <mvasilkov@gmail.com>
2014-09-13 12:53:20 +0300

    2048

---
+++ 2048/main.py
@@ -0,0 +1,30 @@
+from __future__ import division
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.properties import AliasProperty
+from kivy.uix.widget import Widget
+from kivy.utils import get_color_from_hex
+
+SPACING = 10
+
+
+class Game2048Board(Widget):
+    def get_cell_size(self):
+        size = 0.25 * (self.width - 5 * SPACING)
+        return (size, size)
+
+    cell_size = AliasProperty(get_cell_size, None, bind=('width', ))
+
+    # def cell_pos(self, board_x, board_y):
+    #     cs = self.cell_size()
+    #     return (board_x * (cs[0] + SPACING) + self.x,
+    #             board_y * (cs[1] + SPACING) + self.y)
+
+
+class Game2048App(App):
+    pass
+
+if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex('faf8ef')
+    Game2048App().run()
