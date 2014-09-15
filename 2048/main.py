d035418e2c1ce83898fd34ae8572f9864c760537
Mark Vasilkov <mvasilkov@gmail.com>
2014-09-15 20:38:34 +0300

    Changes

--- 2048/main.py
+++ 2048/main.py
@@ -2,27 +2,40 @@ from __future__ import division
 
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.properties import AliasProperty
+from kivy.graphics import Color, BorderImage
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 
-SPACING = 15
+spacing = 15
+
+
+def all_cells():
+    for x in range(0, 4):
+        for y in range(0, 4):
+            yield (x, y)
 
 
 class Game2048Board(Widget):
-    def get_cell_size(self):
-        size = 0.25 * (self.width - 5 * SPACING)
-        return (size, size)
+    def __init__(self, **kwargs):
+        super(Game2048Board, self).__init__(**kwargs)
+        self.refresh()
 
-    def get_cell_pos(self):
-        size = self.get_cell_size()
-        return [[(self.x + SPACING + board_x * (size[0] + SPACING),
-                  self.y + SPACING + board_y * (size[1] + SPACING))
-                 for board_y in range(0, 4)]
-                for board_x in range(0, 4)]
+    def cell_pos(self, board_x, board_y):
+        return (self.x + board_x * (self.cell_size[0] + spacing) + spacing,
+                self.y + board_y * (self.cell_size[1] + spacing) + spacing)
 
-    cell_size = AliasProperty(get_cell_size, None, bind=['width'])
-    cell_pos = AliasProperty(get_cell_pos, None, bind=['cell_size'])
+    def refresh(self, *args):
+        self.cell_size = (0.25 * (self.width - 5 * spacing), ) * 2
+        self.canvas.before.clear()
+        with self.canvas.before:
+            BorderImage(pos=self.pos, size=self.size, source='board.png')
+            Color(*get_color_from_hex('ccc0b4'))
+            for board_x, board_y in all_cells():
+                BorderImage(pos=self.cell_pos(board_x, board_y),
+                            size=self.cell_size, source='cell.png')
+
+    on_pos = refresh
+    on_size = refresh
 
 
 class Game2048App(App):
