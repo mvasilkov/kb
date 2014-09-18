84dc17dae6b505101a64eda12a46b5fd423a4b9f
Mark Vasilkov <mvasilkov@gmail.com>
2014-09-18 22:17:33 +0300

    Changes

--- 2048/main.py
+++ 2048/main.py
@@ -1,8 +1,10 @@
 from __future__ import division
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.graphics import Color, BorderImage
+from kivy.properties import ListProperty, NumericProperty
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
 
@@ -17,6 +19,26 @@ tile_colors = {2 ** (i + 1): colors[i]
                for i in range(len(colors))}
 
 
+class Tile(Widget):
+    font_size = NumericProperty(24)
+    number = NumericProperty(2)
+    color = ListProperty(get_color_from_hex(tile_colors[2]))
+    number_color = ListProperty(get_color_from_hex('776e65'))
+
+    def __init__(self, number=2, **kwargs):
+        super(Tile, self).__init__(**kwargs)
+        self.font_size = 0.5 * self.width
+        self.number = number
+        self.color = get_color_from_hex(tile_colors[number])
+        if self.number > 4:
+            self.number_color = get_color_from_hex('f9f6f2')
+
+    def resize(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.font_size = 0.5 * self.width
+
+
 def all_cells():
     for x in range(4):
         for y in range(4):
@@ -24,16 +46,29 @@ def all_cells():
 
 
 class Board(Widget):
+    b = None
+
     def __init__(self, **kwargs):
         super(Board, self).__init__(**kwargs)
-        self.refresh()
+        self.resize()
+
+    def reset(self):
+        self.b = [[None for i in range(4)] for j in range(4)]
+        Clock.schedule_once(self.new_tile, 0.1)
+
+    def new_tile(self, *args):
+        tile = Tile(pos=self.cell_pos(1, 1), size=self.cell_size)
+        self.b[1][1] = tile
+        self.add_widget(tile)
 
     def cell_pos(self, board_x, board_y):
         return (self.x + board_x * (self.cell_size[0] + spacing) + spacing,
                 self.y + board_y * (self.cell_size[1] + spacing) + spacing)
 
-    def refresh(self, *args):
+    def resize(self, *args):
         self.cell_size = (0.25 * (self.width - 5 * spacing), ) * 2
+
+        # redraw background
         self.canvas.before.clear()
         with self.canvas.before:
             BorderImage(pos=self.pos, size=self.size, source='board.png')
@@ -42,12 +77,23 @@ class Board(Widget):
                 BorderImage(pos=self.cell_pos(board_x, board_y),
                             size=self.cell_size, source='cell.png')
 
-    on_pos = refresh
-    on_size = refresh
+        # resize tiles
+        if not self.b:
+            return
+        for board_x, board_y in all_cells():
+            tile = self.b[board_x][board_y]
+            if tile:
+                tile.resize(pos=self.cell_pos(board_x, board_y),
+                            size=self.cell_size)
+
+    on_pos = resize
+    on_size = resize
 
 
 class GameApp(App):
-    pass
+    def on_start(self):
+        self.root.ids.board.reset()
+
 
 if __name__ == '__main__':
     Window.clearcolor = get_color_from_hex('faf8ef')
